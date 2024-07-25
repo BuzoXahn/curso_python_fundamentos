75629f30628ef8141a1e7614d2429b79b39decf1
AlanB14 <alanbgarcia23@gmail.com>
2024-07-24 20:54:23 -0600

    Ahi va un intento de cambio

---
+++ variables_de_datos_alan.py
@@ -0,0 +1,23 @@
+# Tipo de dato cadena/str
+
+Alumno = "Alan"
+
+# tipo de dato entero/int
+
+Edad = 20
+
+'''Este es un comentario
+de varias lineas'''
+
+# tipo de dato flotante/float
+
+Peso = 100.3
+
+# tipo de dato booleano/bool
+
+nublado = False
+
+print(Alumno)
+print(Edad)
+print(Peso)
+print(nublado)
