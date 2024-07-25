ee8415bf6878a77f783e3c95653fa41733579b42
AlanB14 <alanbgarcia23@gmail.com>
2024-07-24 22:07:03 -0600

    ejercicio 1 de alan

---
+++ ejercicio_1.py
@@ -0,0 +1,8 @@
+#Este programa es ejercicio 1
+
+var1 = input("Cual es tu nombre? ")
+var2 = int(input("Cual es tu edad? "))
+var3 = input("Cual es tu libro favorito? ")
+var4 = input("Cual es tu deporte Favorito? ")
+
+print(" El alumno es ", var1, "\n Tiene ", var2, "años ", "\n Su libro favorito es ", var3, "\n Y el deporte que mas le gusta es ", var4)
