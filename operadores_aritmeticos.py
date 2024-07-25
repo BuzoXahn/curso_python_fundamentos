c24bdaf2b9f484c27ca6de60035926bc99e574fe
GusLimon98 <limonesr.1993@hotmail.com>
2024-07-24 22:22:59 -0600

    ejercicio del 24 de Junio

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,6 @@
+nombre= input("Ingresa tu nombre: ")
+edad= int(input("Ingresa tu edad: "))
+libro= input("Ingresa tu libro favorito: ")
+deporte= input("Ingresa tu deporte favorito: ")
+print("su nombre es ", nombre, "su edad es ", edad, "su libro favorito es", "su deporte favorito es",deporte)
+
