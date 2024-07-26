2db7e69f4e6069d72dc1028cfaddea39d26abf4d
AlanB14 <alanbgarcia23@gmail.com>
2024-07-25 22:51:08 -0600

    Tarea 1 de Alan

---
+++ tarea1_alan.py
@@ -0,0 +1,14 @@
+altura = float(input("\n Ingresa tu estatura en metros: \n"))
+peso = float(input("\n Ingresa tu peso en kilos \n"))
+
+IMC = peso / altura **2
+
+if IMC < 18.5:
+    print("\n Tu IMC es de: ", round(IMC,2) , "\n Estas falto de peso\n")
+elif (IMC >= 18.5 and IMC < 24.9):
+        print("\n Tu IMC es de: ", round(IMC,2) , "\n Estas en tu peso ideal\n")
+elif (IMC >= 24.9 and IMC < 29.9 ):
+    print("\n Tu IMC es de: ", round(IMC,2) ,"\n Tienes Sobrepeso" )
+else:
+    print("\n Tu IMC es de: ", round(IMC,2) , "\n Tienes Obesidad\n A correr")
+        
