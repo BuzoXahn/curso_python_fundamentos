e10d2ca7d131f4d43edcef53d774730e1986f2b0
AlanB14 <alanbgarcia23@gmail.com>
2024-07-25 23:07:53 -0600

    Tarea Corregida

--- tarea1_alan.py
+++ tarea1_alan.py
@@ -3,12 +3,15 @@ peso = float(input("\n Ingresa tu peso en kilos \n"))
 
 IMC = peso / altura **2
 
-if IMC < 18.5:
-    print("\n Tu IMC es de: ", round(IMC,2) , "\n Estas falto de peso\n")
-elif (IMC >= 18.5 and IMC < 24.9):
-        print("\n Tu IMC es de: ", round(IMC,2) , "\n Estas en tu peso ideal\n")
-elif (IMC >= 24.9 and IMC < 29.9 ):
-    print("\n Tu IMC es de: ", round(IMC,2) ,"\n Tienes Sobrepeso" )
+if(IMC < 18.5):
+    print( "\n Tu IMC es de: ", (IMC) , "\n Estas falto de peso\n")
 else:
-    print("\n Tu IMC es de: ", round(IMC,2) , "\n Tienes Obesidad\n A correr")
-        
+    if(IMC >=18.5 and IMC < 24.9):
+        print("\n Tu IMC es de: ", (IMC) , "\n Estas en tu peso ideal\n")
+    else:
+        if(IMC >= 24.9 and IMC <29.9 ):
+            print("\n Tu IMC es de: ", (IMC) ,"\n Tienes Sobrepeso" )
+        else: 
+            print("\n Tu IMC es de: ", (IMC) , "\n Tienes Obesidad\n A correr")
+
+
