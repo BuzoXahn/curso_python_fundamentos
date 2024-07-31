1f93761adb7be85bdefdcc548395e4fec0994552
GusLimon98 <limonesr.1993@hotmail.com>
2024-07-30 22:23:14 -0600

    ejercicio limon corrdores

---
+++ loops.py
@@ -0,0 +1,6 @@
+for x in range (1,101):
+    corredor= input ("Nombre: ")
+    correo= input ("Correo: ")
+    print ("Corredor numero -> " + str(x))
+    print ("Bienvenido, ya etsas incrito a la carrera. Te enviaremos la confirmacion a tu Correo"  + correo)
+  
